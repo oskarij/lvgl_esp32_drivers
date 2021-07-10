25c019fb6467c0da9413ce09370c5364aca07ec2
Nineis K <is_kk@hotmail.com>
2021-07-10 23:55:33 +0700

    Make it work with V8
    LVGL V8 remove LV_HOR_RES_MAX, LV_HOR_RES_MAX from ./lvgl/src/lv_conf_internal.h

--- lvgl_helpers.h
+++ lvgl_helpers.h
@@ -22,6 +22,22 @@ extern "C" {
  *      DEFINES
  *********************/
 
+/* Maximal horizontal and vertical resolution to support by the library.*/
+#ifndef LV_HOR_RES_MAX
+#  ifdef CONFIG_LV_HOR_RES_MAX
+#    define LV_HOR_RES_MAX CONFIG_LV_HOR_RES_MAX
+#  else
+#    define  LV_HOR_RES_MAX          (480)
+#  endif
+#endif
+#ifndef LV_VER_RES_MAX
+#  ifdef CONFIG_LV_VER_RES_MAX
+#    define LV_VER_RES_MAX CONFIG_LV_VER_RES_MAX
+#  else
+#    define  LV_VER_RES_MAX          (320)
+#  endif
+#endif
+  
 /* DISP_BUF_SIZE value doesn't have an special meaning, but it's the size
  * of the buffer(s) passed to LVGL as display buffers. The default values used
  * were the values working for the contributor of the display controller.
